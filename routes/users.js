7fb1884bc34bfa6bb92b186e8f10e600c63efef3
Alexander Artemiev <alexanderma1983@gmail.com>
2026-02-15 11:43:50 -0500

    Actualizacion del backend

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,7 @@ router.get('/me', getUserMe);
 //router.patch('/me/picture', updatePicture);
 router.get('/:userId', getUserById);
 router.post('/signup', createUser);
-router.patch('/me/avatar', updateAvatar);
+//router.patch('/me/avatar', updateAvatar);
 router.patch('/me', updateUser);
 
 module.exports = router;
