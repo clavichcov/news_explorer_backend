75a56fd2e433575050aea584ccbdc1f86ebd4fda
Alexander Artemiev <alexanderma1983@gmail.com>
2026-02-15 11:25:31 -0500

    Actualizacion del backend

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
 const router = require('express').Router();
-const { getUserById, getUsers, getUserMe, updatePicture
-         } = require('../controllers/users');
+const { getUserById, getUsers, createUser, 
+    updateUser, updateAvatar, getUserMe } = require('../controllers/users');
 
-router.get('/', getUsers);
+router.get('/signin', getUsers);
 router.get('/me', getUserMe);
 //router.patch('/me/picture', updatePicture);
 router.get('/:userId', getUserById);
+router.post('/signup', createUser);
+router.patch('/me/avatar', updateAvatar);
+router.patch('/me', updateUser);
+
 module.exports = router;
